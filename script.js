25130eba1f10c795e632daaa13c130ed9c9ff33a
Shawky Elsayed <shawkyelsayed2002@gmail.com>
2025-02-11 20:32:59 +0200

    halfway

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // import gsap from 'gsap';
+
+
 document.addEventListener("DOMContentLoaded", () => {
+
   const t1 = gsap.timeline({ repeat: -1, defaults: { duration: 1 } });
 
   gsap.utils.toArray(".text-anim h1").forEach((text, index) => {
+
     t1.to(text, { top: 0 }, '>') // Stagger effect
       .to(text, { top: "-100%" }, ">");
   });
